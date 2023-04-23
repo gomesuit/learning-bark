971fc58b4bd8717a0ed2f98ad546ec69e0c50002
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-23 20:52:40 +0900

    save

--- samples/pizza.py
+++ samples/pizza.py
@@ -9,7 +9,7 @@ preload_models()
 
 # generate audio from text
 text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+     Hello, my name is Suno. And, uh — and I like pizza. [laughs]
      But I also have other interests such as playing tic tac toe.
 """
 audio_array = generate_audio(text_prompt)
